d9047f0af770f2920440c1695f7bfde467a0f230
Justin Holewinski <justin.holewinski@gmail.com>
2012-01-05 08:25:40 -0500

    Add OCL Jacobi 2D skeleton

--- opencl/common/inc/CLCommon.hpp
+++ opencl/common/inc/CLCommon.hpp
@@ -2,6 +2,11 @@
 #ifndef CLCOMMON_HPP_INC
 #define CLCOMMON_HPP_INC 1
 
+// C++ headers
 #include <string>
+#include <stdexcept>
+
+// CL headers
+#include "cl.hpp"
 
 #endif
---
+++ opencl/jacobi-2d/jacobi-2d.cpp
@@ -0,0 +1,9 @@
+
+#include "CLCommon.hpp"
+#include "ProgramGenerator.hpp"
+
+
+
+int main(int argc, char** argv) {
+  return 0;
+}
